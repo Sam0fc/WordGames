179003ea8bea5401282d4294b972487841bae30f
Sam0fc <sammyhutton@gmail.com>
2020-09-10 19:06:52 -0700

    Added core game loop of the player v player game

--- ghostGame.py
+++ ghostGame.py
@@ -1,24 +1,42 @@
-scrabbleDictPath = "Scrabble.txt"
 
-def LoadWords():
-	scrabbleFile = open(scrabble, "r")
+
+def LoadWords(scrabbleDictPath):
+	scrabbleFile = open(scrabbleDictPath, "r")
 	wordList = []
 	for line in scrabbleFile:
 		wordList.append(line)
 	return wordList
 
-def doPlayerTurn(gameString, wordList, playerId):
-    commandString = input("").lower()
-    if commandString == "challenge":
-        doChallenge(gameString, wordList)
-    if gameString in wordList:
-        doLoseGame(playerId)
+def doPlayerTurn(gameString, wordList, playerOne, playerTwo):
+    done = False
+    while not done:
+        commandString = input("Enter a letter, or #c for challenge: \n").lower()
+        if commandString == "#c":
+            if doChallenge(gameString, wordList):
+                doLoseGame(playerTwo)
+            else:
+                doLoseGame(playerOne)
+        elif commandString.length() == 1:
+            gameString+=commandString
+            if gameString in wordList:
+                doLoseGame(playerOne)
+                done = True
+        else:
+            print("Invalid Input, Try Again")
+        print(gameString.upper())
 
 def doLoseGame(playerId):
     print("Game Over, Player " + playerId + " loses")
 
-def doChallenge(gameString, wordList, playerId):
+def doChallenge(gameString, wordList):
     for word in wordList:
-        if word[0:(gameString.length())] == gameString:
-            print("Challenge Failed")
-            doLoseGame(playerId)
+        if word[:(gameString.length())] == gameString:
+            return False
+        else:
+            return True
+##############################################################################
+scrabbleDictPath = "Scrabble.txt"
+gameString = ""
+wordList = LoadWords(scrabbleDictPath)
+playerOne, playerTwo = doSetup()
+doPlayerTurn(gameString, wordList, )
